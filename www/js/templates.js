801bda6b8f475be558d57a397c201992fdf9f8d6
Jon Sherard <jonsherrard@gmail.com>
2012-12-19 16:21:53 +0000

    minor change

--- www/js/templates.js
+++ www/js/templates.js
@@ -5,5 +5,5 @@ templates['home.html'] = template(function (Handlebars,depth0,helpers,partials,d
   
 
 
-  return "<div class=\"container\"><div class=\"row\"><div class=\"span12\"><div class=\"hero-unit\"><h1>Web Audio Oscillator</h1></div></div></div></div>";});
+  return "<div class=\"container\"><div class=\"row\"><div class=\"span12\"><div class=\"hero-unit\"><h1>Web Audio Oscillator</h1></div></div></div><div class=\"row\"><div id=\"controls\"><span6 class=\"offset3\"><button class=\"btn btn-success\">Play</button><button class=\"btn btn-danger\">Stop</button></span6></div></div></div>";});
 })();
