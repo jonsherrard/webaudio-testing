42c14de25913738ad64a40aec120a15affcc81ed
Jon Sherard <jonsherrard@gmail.com>
2012-12-19 16:43:18 +0000

    minor change

--- www/js/templates.js
+++ www/js/templates.js
@@ -5,5 +5,5 @@ templates['home.html'] = template(function (Handlebars,depth0,helpers,partials,d
   
 
 
-  return "<div class=\"container\"><div class=\"row\"><div class=\"span12\"><div class=\"hero-unit\"><h1>Web Audio Oscillator</h1></div></div></div><div class=\"row\"><div id=\"controls\"><div class=\"span1 offset2\"><button id=\"play\" class=\"btn btn-success\">Play</button></div><div class=\"span1 offset1\"><button id=\"stop\" class=\"btn btn-danger\">Stop</button></div><div class=\"span1 offset1\"><input id=\"freq\" placeholder=\"2000\" type=\"text\"/></div></div></div></div>";});
+  return "<div class=\"container\"><div class=\"row\"><div class=\"span12\"><div class=\"hero-unit\"><h1>Web Audio Oscillator</h1></div></div></div><div class=\"row\"><div id=\"controls\"><div class=\"span1 offset2\"><button id=\"play\" class=\"btn btn-success\">Play</button></div><div class=\"span1 offset1\"><button id=\"stop\" class=\"btn btn-danger\">Stop</button></div><div class=\"span1 offset1\"><label for=\"freq\">Frequency</label><input id=\"freq\" placeholder=\"2000\" name=\"freq\" type=\"text\"/></div></div></div></div>";});
 })();
